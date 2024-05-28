42e0bbf625c6b54b83b798b4490693a1ca16d910
Opiyo24 <gabbynyangor@gmail.com>
2024-05-28 08:08:04 +0300

    Fully functional navbar

--- engage/accounts/urls.py
+++ engage/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='accounts-home'),
+    path('', views.home, name='home'),
     # path('signup/', views.signup, name='accounts-signup'),
     path('company/', views.company_creation, name='company-signup'),
     path('company_login/', views.login_view, name='company-login'),
